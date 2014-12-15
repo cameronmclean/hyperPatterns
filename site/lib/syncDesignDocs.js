cda49087501f49a9e0b4712f7dda60cf8b746bf0
cameronmclean <ca.mclean@auckland.ac.nz>
2014-12-15 16:42:42 +1300

    created lib for storing and syncing couchDB design docs

---
+++ site/lib/syncDesignDocs.js
@@ -0,0 +1,28 @@
+// run node syncDesignDocs.js from the lib folder to update/add the views and design docs that we define for couchDB here 
+// note relative path to nano module 
+
+var nano = require('../node_modules/nano')('http://127.0.0.1:5984');
+var db = nano.use('patterns');
+
+// view to return all the pattern docs for a /patterns/ GET
+var patternDesignDoc = {
+	_id: '_design/patterns',
+	language: 'javascript',
+	'views': {
+		'getAllPatterns': {
+			"map": "function(doc){emit(doc_id, doc._rev)}"
+		}
+
+	}
+
+} ;
+
+db.insert(patternDesignDoc, '_design/patterns', function(err, body){
+	if (!err){
+		console.log("Design docs updated!");
+	}
+
+	else {
+		console.log(err);
+	};
+});
