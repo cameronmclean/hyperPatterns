7ebdf9b070cf75862098c7e8fde3d59d23b09315
cameronmclean <ca.mclean@auckland.ac.nz>
2015-01-20 14:32:55 +1300

    fixed syncContextDocs.js and added comments

--- site/lib/syncContextDocs.js
+++ site/lib/syncContextDocs.js
@@ -1,5 +1,7 @@
-// run node syncContextDocs.js from the lib folder to update/add the context docs that we specify here 
-// note relative path to nano module 
+// run `node syncContextDocs.js` from the lib folder to update/add the context docs that we specify here 
+// note the relative path to nano module..
+// currently this script requires the context docs to already exist in couch 
+// if creating/defining a brand new context doc - use futon or curl to create an otherwise empty doc with the needed "_id" first.
 
 var nano = require('../node_modules/nano')('http://127.0.0.1:5984');
 var db = nano.use('patterns');
@@ -77,29 +79,41 @@ var forceContext = {
 	}
 };
 
-//first get the desgin doc and grab the rev
-//NB assumes the design doc already exists!!!
-//TODO - handle create if not exist?
-var getRev = db.get("bibTEX", function(err, body){
-	// if not error - 
-	if (!err){
-		console.log('bibTEX context doc exists with... _rev: '+ body['_rev']);
-		//double check that body['_rev'] exists
-		if (body['_rev']){
-			//set _rev on patternDesignDoc specfied above to current _rev we just fetched
-			bibTEXContext['_rev'] = body['_rev'];
-			
-			//update the existing context doc
-			db.insert(bibTEXContext, 'bibTEX', function(err, body){
-			if (!err){
-				console.log("bibTEX context doc updated!");
-			}
+var contextDocs = [bibTEXContext, contributorContex, patternContext, forceContext];
 
-			else {
-				console.log(err);
-			}
-		    });
-	    }
-		
+//note this function uses an Immediately Invoked Function expression to 
+// allow async call-back funtions to close properly within the 
+// for loop. see
+// http://stackoverflow.com/questions/750486/javascript-closure-inside-loops-simple-practical-example/19323214#19323214
+// http://learn.jquery.com/javascript-101/functions/#immediately-invoked-function-expression-iife
+// http://en.wikipedia.org/wiki/Immediately-invoked_function_expression
+
+function syncDocs() {
+	for (var x = 0; x < contextDocs.length; x++) {
+		//IIFE - anon function will be called on each iteration of the for loop
+		// we pass in the value of for loop x as index within the anon funct 
+		(function(index){
+			//we copy the contents of the JSON objects specified above into the temp var doc here
+			var doc = JSON.parse(JSON.stringify(contextDocs[index]));
+			//retreive the doc from couch db
+			db.get(doc['_id'], function(err, body){
+				if(!err){
+					//if OK, set/create temp doc "_rev" field to match current db rev
+					doc['_rev'] = body['_rev'];
+					//write the doc
+					db.insert(doc, function(err, body){
+						console.log(body);
+					})
+				}
+				else{
+					console.log(err);
+				}
+				//console.log("doc id is "+doc['_id']+" and doc rev is set to "+doc['_rev']);
+			})
+		})(x); // we send the for loop iterator x to the (IIFE) anon function above, where it is defined as 'index' 
+			   // see IIFE links above
 	}
-});
+}
+
+
+syncDocs();
